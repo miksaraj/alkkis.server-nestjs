11b8738ee6174ce956e8327a068671d63da57d04
Miksa <mikko.s.rajakangas@gmail.com>
2021-10-13 21:21:11 +0300

    fixed bac representation, enabled cors

--- src/bac/bac.controller.ts
+++ src/bac/bac.controller.ts
@@ -1,6 +1,7 @@
 import { Body, Controller, Post } from '@nestjs/common';
 import { BacService } from './bac.service';
 import { BacRequestDto } from './dto/bac-request.dto';
+import { BacRepresentation } from './functions/bac.functions';
 
 @Controller({
     path: 'bac',
@@ -10,7 +11,7 @@ export class BacController {
     constructor(private bacService: BacService) {}
 
     @Post()
-    returnBac(@Body() bacRequestDto: BacRequestDto): string {
+    returnBac(@Body() bacRequestDto: BacRequestDto): BacRepresentation {
         return this.bacService.getBac(bacRequestDto)
     }
 }
--- src/bac/bac.service.ts
+++ src/bac/bac.service.ts
@@ -1,12 +1,12 @@
 import { Injectable } from "@nestjs/common";
 import { BacRequestDto } from "./dto/bac-request.dto";
-import { calculateBac, getBacRepresentation } from './functions/bac.functions'
+import { BacRepresentation, calculateBac, getBacRepresentation } from './functions/bac.functions'
 
 @Injectable()
 export class BacService {
     constructor() {}
 
-    getBac(data: BacRequestDto): string {
+    getBac(data: BacRequestDto): BacRepresentation {
         return getBacRepresentation(calculateBac(
             data.products,
             data.drinker
